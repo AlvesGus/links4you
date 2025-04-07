11132797f6154f27ead39cfdc45552f33fc613c1
AlvesGus <gustavo.tbt@hotmail.com.br>
2025-04-06 21:00:36 -0300

    fix: layout.tsx file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="w-full h-dvh flex">
       <div className="w-full lg:max-w-[80%] lg:m-auto px-4 py-10 lg:py-0 ">
         <div className="w-full flex items-center justify-center lg:justify-start gap-2 mb-10">
-          <Link size={30} />
+          <Link size={28} />
           <span className="text-4xl">Brev.ly</span>
         </div>
         <div className="w-full flex flex-col lg:flex-row lg:gap-10 gap-4">
